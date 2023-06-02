ae7d56f81afea92c0cc274a74f35a3fb8844e88d
Michelle Xie <93167396+mxie97@users.noreply.github.com>
2023-06-02 15:26:46 -0700

    error handling

--- script.js
+++ script.js
@@ -41,20 +41,46 @@ numberButtons.forEach((numberButton) => {
     numberButton.addEventListener('click', populateDisplay)
 })
 
-equalsButton.addEventListener('click', evaluateDisplay)
-
 function populateDisplay() {
     let input = this.textContent;
+
     if (input === '+' || input === '-' || input === 'x' || input === '÷') {
+        // check first if operator already exists
+        if (operator) {
+            evaluateDisplay();
+        }
         a = parseInt(display.textContent);
         operator = this.id;
     } 
     display.textContent = display.textContent + input;
 }
 
+equalsButton.addEventListener('click', evaluateDisplay)
+
 function evaluateDisplay() {
     let bStartIndex = a.toString().length + 1;
     b = parseInt(display.textContent.slice(bStartIndex));
-    result = operate(a, operator, b);
-    display.textContent = result;
+    if (a === undefined || operator === undefined || b === undefined) {
+        display.textContent = 'ERROR - press CLEAR to continue';
+    } else if (operator === 'divide' && b === 0) {
+        display.textContent = '>:( no dividing by 0 allowed - press CLEAR to continue';
+    } else {
+        let result = operate(a, operator, b);
+        display.textContent = result;
+        resetVariables();
+    }
+    
+}
+
+function resetVariables() {
+    a = undefined;
+    b = undefined;
+    operator = undefined;
+}
+
+clearButton.addEventListener('click', reset)
+
+function reset() {
+    resetVariables();
+    display.textContent = '';
 }
